1355c3a639c319f81fdabec37377509dbb8eb5b2
josepainumkal <josepainumkal@users.noreply.github.com>
2016-09-13 21:37:00 -0700

    Add files via upload

---
+++ Assignment1/myEval.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+// This function takes a vector as input
+// returns 100, only when input vector contains the bit value 1 at all index positions that are
+// multiples of 13 and rest of the bits are 0.
+// For all other vectors, the return value will be 50
+
+double eval(int *vec){
+	int sumOfPrimeOnes;
+	int sumOfZeroes;
+
+	for(int i=0;i<100;i++){
+        if(vec[i]==0){
+            sumOfZeroes++;
+        }
+		if((i%13)==0){
+              if(vec[i]==1){
+                 sumOfPrimeOnes++;
+              }
+		}
+	}
+
+	if((sumOfZeroes+sumOfPrimeOnes)==100){
+          return 100; 
+	}else{
+		return 50;
+	}
+}
+
